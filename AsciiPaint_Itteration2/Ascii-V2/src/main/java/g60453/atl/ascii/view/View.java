32f627059f37f97a7ed392186ac827b301690fd9
Marcel Angielczyk <ixarmc@gmail.com>
2024-03-15 22:02:37 +0100

    help command updated

--- AsciiPaint_Itteration2/Ascii-V2/src/main/java/g60453/atl/ascii/view/View.java
+++ AsciiPaint_Itteration2/Ascii-V2/src/main/java/g60453/atl/ascii/view/View.java
@@ -7,15 +7,17 @@ public class View {
     public static void help() {
         String helpMessage = "\u001B[38;5;51mCOMMANDS:\u001B[0m\n" +
                 "\u001B[38;5;46m. Add [shape] [parameters]\u001B[0m => Add a shape to the drawing. Parameters depend on the shape.\n" +
-                "\u001B[38;5;46m    - Circle [x] [y] [radius] [color]\u001B[0m => Add a circle. x and y are the coordinates of the center, radius is the radius of the circle, color is the color of the circle.\n" +
-                "\u001B[38;5;46m    - Rectangle [x] [y] [width] [height] [color]\u001B[0m => Add a rectangle. x and y are the coordinates of the upper left corner, width and height are the dimensions of the rectangle, color is the color of the rectangle.\n" +
-                "\u001B[38;5;46m    - Square [x] [y] [side] [color]\u001B[0m => Add a square. x and y are the coordinates of the upper left corner, side is the length of the side of the square, color is the color of the square.\n" +
                 "\u001B[38;5;46m. Show\u001B[0m => Display the current state of the drawing\n" +
                 "\u001B[38;5;46m. List\u001B[0m => List all shapes in the drawing\n" +
                 "\u001B[38;5;46m. Move [index] [dx] [dy]\u001B[0m => Move a shape in the drawing. Index is the shape's position in the list, dx and dy are the displacement in x and y directions respectively.\n" +
                 "\u001B[38;5;46m. Color [index] [color]\u001B[0m => Change the color of a shape in the drawing. Index is the shape's position in the list, color is the new color.\n" +
+                "\u001B[38;5;46m. Group [index1] [index2] ... [indexN]\u001B[0m => Group shapes in the drawing. Indexes are the positions of the shapes in the list.\n" +
+                "\u001B[38;5;46m. Ungroup [index]\u001B[0m => Ungroup a group of shapes in the drawing. Index is the position of the group in the list.\n" +
+                "\u001B[38;5;46m. Delete [index]\u001B[0m => Delete a shape from the drawing. Index is the shape's position in the list.\n" +
+                "\u001B[38;5;46m. Undo\u001B[0m => Undo the last command\n" +
                 "\u001B[38;5;46m. Exit\u001B[0m => Exit the program\n" +
                 "\u001B[38;5;46m. Help\u001B[0m => Display this help message\n";
+
         System.out.println(helpMessage);
     }
 
@@ -97,6 +99,7 @@ public class View {
     public static void ungroupSuccess() {
         System.out.println("\u001B[38;5;51mShapes ungrouped successfully\u001B[0m");
     }
+
 
     public static void exit() {
         System.out.println("\u001B[38;5;196mshutting down...\u001B[0m");
